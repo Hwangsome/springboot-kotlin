0693a4e98e7c33f998ead574bbb172ac2752f28f
bhuang <v-bhuang@expediagroup.com>
2022-09-22 14:57:30 +0800

    第一次初始化springboot , kotlin

---
+++ kotlin-1/src/test/kotlin/com/bh/kotlin1/Kotlin1ApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.bh.kotlin1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlin1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
